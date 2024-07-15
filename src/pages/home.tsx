e0b70986a93ab7035c88d068fd129b5e364cd9a3
Aman Kumar <amankarguwal0@gmail.com>
2024-07-15 23:45:52 +0530

    feat: Add lazy loading for images on home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import ProductCard from "../components/ProductCard";
 
 const Home = () => {
-  return (
-    <div>
-      Home
-    </div>
-  )
-}
 
-export default Home
+  const addToCartHandler = (productId: string) => {}
+
+  return (
+    <div className="home">
+      <section></section>
+      <h1>
+        Latest Products
+        <Link to="/search" className="findMore">
+          More
+        </Link>
+      </h1>
+      <main>
+        <ProductCard
+          productId="asdhkadsf"
+          photo="618d5bS2lUL._SX679_.jpg"
+          name="Macbook Pro"
+          price={100000}
+          stock={100}
+          handler={addToCartHandler}
+        />
+      </main>
+    </div>
+  );
+};
+
+export default Home;
