4a9b9f782a6cf3dd62bc9d2d21c128b8a1719b4d
Aman Kumar <amankarguwal0@gmail.com>
2024-07-15 23:57:44 +0530

    refactor: Update cart page with coupon code functionality and styling

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,11 +1,95 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { VscError } from "react-icons/vsc";
+import CartItem from "../components/CartItem";
+import { Link } from "react-router-dom";
+
+const cartItems = [
+  {
+    productId: "adasdasd",
+    photo: "https://m.media-amazon.com/images/I/618d5bS2lUL._SX679_.jpg",
+    name: "Macbook Pro",
+    price: 3000,
+    quantity: 4,
+    stock: 10,
+  },
+];
+
+const subtotal = 4000;
+const tax = Math.round(0.18 * subtotal);
+const shippingCharges = 200;
+const discount = 500;
+const total = subtotal + tax + shippingCharges;
 
 const Cart = () => {
-  return (
-    <div>
-      Cart
-    </div>
-  )
-}
+  const [couponCode, setCouponCode] = useState<string>("");
+  const [isValidCouponCode, setIsValidCouponCode] = useState<boolean>(false);
 
-export default Cart
+  useEffect(() => {
+    const id = setTimeout(() => {
+      if (Math.random() > 0.5) {
+        setIsValidCouponCode(true);
+      } else {
+        setIsValidCouponCode(false);
+      }
+    }, 1000);
+
+    return () => {
+      clearTimeout(id);
+      setIsValidCouponCode(false);
+    };
+  }, [couponCode]);
+
+  return (
+    <div className="cart">
+      <main>
+        {cartItems.length > 0 ? (
+          cartItems.map((item, index) => (
+            <CartItem
+              key={index}
+              productId={item.productId}
+              photo={item.photo}
+              name={item.name}
+              price={item.price}
+              quantity={item.quantity}
+              stock={item.stock}
+            />
+          ))
+        ) : (
+          <h1>No Items Added</h1>
+        )}
+      </main>
+      <aside>
+        <p>Subtotal: ₹{subtotal}</p>
+        <p>Shipping Charges: ₹{shippingCharges}</p>
+        <p>Tax: ₹{tax}</p>
+        <p>
+          Discount: <em className="red"> - ₹{discount}</em>
+        </p>
+        <p>
+          <b>Total: ₹{total}</b>
+        </p>
+        <input
+          type="text"
+          value={couponCode}
+          placeholder="Coupon Code"
+          onChange={(e) => setCouponCode(e.target.value)}
+        />
+
+        {couponCode &&
+          (isValidCouponCode ? (
+            <span className="green">
+              ₹{discount} off using the <code>{couponCode}</code>
+            </span>
+          ) : (
+            <span className="red">
+              Invalid Coupon <VscError />
+            </span>
+          ))}
+
+        {cartItems.length > 0 && <Link to="/shipping">Checkout</Link>}
+      </aside>
+    </div>
+  );
+};
+
+export default Cart;
