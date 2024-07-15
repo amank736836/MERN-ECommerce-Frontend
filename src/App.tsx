42124678fad861be634d53cc0085c6a9bb08c5ef
Aman Kumar <amankarguwal0@gmail.com>
2024-07-15 16:57:59 +0530

    refactor: Optimize code splitting and lazy loading for admin dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,85 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { lazy, Suspense } from "react";
-import Loader from "./components/loader";
+import { Link, Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Loader from "./components/admin/Loader";
+
 const Home = lazy(() => import("./pages/home"));
 const Search = lazy(() => import("./pages/search"));
 const Cart = lazy(() => import("./pages/cart"));
+
+import NewProduct from "./pages/admin/management/NewProduct";
+import ProductManagement from "./pages/admin/management/ProductManagement";
+import TransactionManagement from "./pages/admin/management/TransactionManagement";
+
+const Dashboard = lazy(() => import("./pages/admin/Dashboard"));
+const Product = lazy(() => import("./pages/admin/Product"));
+const Transaction = lazy(() => import("./pages/admin/Transaction"));
+const Customer = lazy(() => import("./pages/admin/Customer"));
+
+const BarCharts = lazy(() => import("./pages/admin/charts/BarCharts"));
+const PieCharts = lazy(() => import("./pages/admin/charts/PieCharts"));
+const LineCharts = lazy(() => import("./pages/admin/charts/LineCharts"));
+
+const Stopwatch = lazy(() => import("./pages/admin/apps/Stopwatch"));
+const Coupon = lazy(() => import("./pages/admin/apps/Coupon"));
+const Toss = lazy(() => import("./pages/admin/apps/Toss"));
 
 const App = () => {
   return (
     <Router>
       {/* <Header/> */}
-      <Suspense fallback={<Loader/>}>
+      <Suspense fallback={<Loader />}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/search" element={<Search />} />
           <Route path="/cart" element={<Cart />} />
+          {/* 
+          <Route
+            element={
+              <ProtectedRoute
+                isAuthenticated={true}
+                adminRoute={true}
+                isAdmin={true}
+              />
+            }
+          > */}
+
+          <Route
+            path="/"
+            element={
+              <Link
+                to="/admin/dashboard"
+                style={{
+                  display: "block",
+                  textAlign: "center",
+                  padding: "1rem",
+                  fontSize: "1.5rem",
+                }}
+              >
+                Visit Dashboard
+              </Link>
+            }
+          />
+
+          <Route path="/admin/dashboard" element={<Dashboard />} />
+          <Route path="/admin/product" element={<Product />} />
+          <Route path="/admin/transaction" element={<Transaction />} />
+          <Route path="/admin/customer" element={<Customer />} />
+
+          <Route path="/admin/chart/bar" element={<BarCharts />} />
+          <Route path="/admin/chart/pie" element={<PieCharts />} />
+          <Route path="/admin/chart/line" element={<LineCharts />} />
+
+          <Route path="/admin/app/stopwatch" element={<Stopwatch />} />
+          <Route path="/admin/app/coupon" element={<Coupon />} />
+          <Route path="/admin/app/toss" element={<Toss />} />
+
+          <Route path="/admin/product/:id" element={<ProductManagement />} />
+          <Route path="/admin/product/add" element={<NewProduct />} />
+          <Route
+            path="/admin/transaction/:id"
+            element={<TransactionManagement />}
+          />
+          {/* </Route> */}
         </Routes>
       </Suspense>
     </Router>
