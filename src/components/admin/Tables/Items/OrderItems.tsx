fc07933b240abe8f90176092a3fce0cc93cb729d
Aman Kumar <amankarguwal0@gmail.com>
2024-07-17 00:14:11 +0530

    refactor: Add OrderItems component for displaying order items in admin dashboard

---
+++ src/components/admin/Tables/Items/OrderItems.tsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+
+export const OrderItems = [
+  {
+    _id: "1",
+    quantity: 2,
+    discount: 10,
+    amount: 200,
+    status: <span className="green">Delivered</span>,
+    action: <Link to={`/orders/1`}>View</Link>,
+  },
+  {
+    _id: "2",
+    quantity: 3,
+    discount: 20, 
+    amount: 300,
+    status: <span className="red">Processing</span>,
+    action: <Link to={`/orders/2`}>View</Link>,
+  },
+  {
+    _id: "3",
+    quantity: 4,
+    discount: 30,
+    amount: 400,
+    status: <span>Delivered</span>,
+    action: <Link to={`/orders/3`}>View</Link>,
+  },
+];
