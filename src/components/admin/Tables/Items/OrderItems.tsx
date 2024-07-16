d8f75abfa5cef617aea9f46e1c6f89cd3956d345
Aman Kumar <amankarguwal0@gmail.com>
2024-07-17 00:15:35 +0530

    refactor: Update OrderItems links in OrderItems component

--- src/components/admin/Tables/Items/OrderItems.tsx
+++ src/components/admin/Tables/Items/OrderItems.tsx
@@ -7,7 +7,7 @@ export const OrderItems = [
     discount: 10,
     amount: 200,
     status: <span className="green">Delivered</span>,
-    action: <Link to={`/orders/1`}>View</Link>,
+    action: <Link to={`/order/1`}>View</Link>,
   },
   {
     _id: "2",
@@ -15,7 +15,7 @@ export const OrderItems = [
     discount: 20, 
     amount: 300,
     status: <span className="red">Processing</span>,
-    action: <Link to={`/orders/2`}>View</Link>,
+    action: <Link to={`/order/2`}>View</Link>,
   },
   {
     _id: "3",
@@ -23,6 +23,6 @@ export const OrderItems = [
     discount: 30,
     amount: 400,
     status: <span>Delivered</span>,
-    action: <Link to={`/orders/3`}>View</Link>,
+    action: <Link to={`/order/3`}>View</Link>,
   },
 ];
