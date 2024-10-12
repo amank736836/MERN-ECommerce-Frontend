e3bd6a17c40d710489064b8e265ebb4773ada666
Aman Kumar <amankarguwal0@gmail.com>
2024-10-12 13:39:11 +0530

    refactor: Update Home component to fetch and display latest products
    - Import react-hot-toast for displaying error toast messages
    - Import SkeletonLoader component for displaying loading skeleton when data is being fetched
    - Import useLatestProductsQuery hook from productAPI to fetch latest products
    - Add useEffect hook to handle error case and display error toast
    - Update ProductCard component to dynamically render product data from fetched products
    - Add loading skeleton when data is being fetched

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,18 @@
+import toast from "react-hot-toast";
 import { Link } from "react-router-dom";
+import { SkeletonLoader } from "../components/loader";
 import ProductCard from "../components/ProductCard";
+import { useLatestProductsQuery } from "../redux/api/productAPI";
+import { useEffect } from "react";
 
 const Home = () => {
+  const { data, isLoading, isError } = useLatestProductsQuery("");
 
-  const addToCartHandler = () => {}
+  useEffect(() => {
+    if (isError) {
+      toast.error("Failed to fetch data");
+    }
+  }, [isError]);
 
   return (
     <div className="home">
@@ -15,14 +24,25 @@ const Home = () => {
         </Link>
       </h1>
       <main>
-        <ProductCard
-          productId="asdhkadsf"
-          photo="618d5bS2lUL._SX679_.jpg"
-          name="Macbook Pro"
-          price={100000}
-          stock={100}
-          handler={addToCartHandler}
-        />
+        {isLoading ? (
+          <SkeletonLoader
+            flexDir="row"
+            height="25rem"
+            width="18.75rem"
+            length={6}
+          />
+        ) : (
+          data?.products.map((product) => (
+            <ProductCard
+              key={product._id}
+              productId={product._id}
+              name={product.name}
+              price={product.price}
+              stock={product.stock}
+              photo={product.photo}
+            />
+          ))
+        )}
       </main>
     </div>
   );
