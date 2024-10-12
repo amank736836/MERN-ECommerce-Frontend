90060c46e65f8029cf8de3ff968d16a67a05fbff
Aman Kumar <amankarguwal0@gmail.com>
2024-10-12 13:34:59 +0530

    refactor: Update ProductCard component to use Redux for state management and add addToCart functionality

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,4 +1,9 @@
 import { FaPlus } from "react-icons/fa";
+import { server } from "../redux/store";
+import { CartItem } from "../types/types";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../redux/reducer/cartReducer";
+import toast from "react-hot-toast";
 
 type ProductCardProps = {
   productId: string;
@@ -6,10 +11,7 @@ type ProductCardProps = {
   name: string;
   price: number;
   stock: number;
-  handler: (productId: string) => void;
 };
-
-const server = "https://m.media-amazon.com/images/I";
 
 const ProductCard = ({
   productId,
@@ -17,15 +19,43 @@ const ProductCard = ({
   name,
   price,
   stock,
-  handler,
 }: ProductCardProps) => {
+  const dispatch = useDispatch();
+
+  const addToCartHandler = (cartItem: CartItem) => {
+    if (cartItem.stock < 1) {
+      return toast.error("Out of Stock");
+    }
+    toast.success("Added to Cart");
+    dispatch(addToCart(cartItem));
+  };
+
   return (
     <div className="productCard">
-      <img src={`${server}/${photo}`} alt="" className="src" />
+      <img
+        src={`${server}/${photo}`}
+        alt=""
+        className="src"
+        style={{
+          margin: `${stock < 1 ? "" : "2rem"}`,
+        }}
+      />
       <p>{name}</p>
+      {stock < 1 ? <span style={{ color: "red" }}>Out of Stock</span> : ""}
       <span>₹{price}</span>
       <div>
-        <button onClick={() => handler(productId)}>
+        <button
+          onClick={() =>
+            addToCartHandler({
+              productId,
+              photo,
+              name,
+              price,
+              quantity: 1,
+              stock,
+            })
+          }
+        >
           <FaPlus />
         </button>
       </div>
