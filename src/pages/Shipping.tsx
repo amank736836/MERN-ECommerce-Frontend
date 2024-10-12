b710f0217a5d4160a8b1e020631ff2f8dec3b767
Aman Kumar <amankarguwal0@gmail.com>
2024-10-12 13:39:37 +0530

    refactor: Update Shipping component to include form submission and dispatch shipping info
    - Add axios for API requests and toast for error handling
    - Import useDispatch and useSelector from react-redux for state management
    - Add submitHandler function to handle form submission and dispatch shipping info
    - Make API request to create Razorpay payment and navigate to payment page
    - Handle error cases and display toast message

--- src/pages/Shipping.tsx
+++ src/pages/Shipping.tsx
@@ -1,6 +1,11 @@
-import { ChangeEvent, useState } from "react";
+import axios from "axios";
+import { ChangeEvent, FormEvent, useState } from "react";
+import toast from "react-hot-toast";
 import { BiArrowBack } from "react-icons/bi";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { saveShippingInfo } from "../redux/reducer/cartReducer";
+import { RootState, server } from "../redux/store";
 
 const Shipping = () => {
   const [shippingInfo, setShippingInfo] = useState({
@@ -11,8 +16,6 @@ const Shipping = () => {
     pinCode: "",
   });
 
-  const navigate = useNavigate();
-
   const changeHandler = (
     e: ChangeEvent<HTMLInputElement | HTMLSelectElement>
   ) => {
@@ -22,16 +25,49 @@ const Shipping = () => {
     }));
   };
 
+  const { total } = useSelector((state: RootState) => state.cartReducer);
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const submitHandler = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    dispatch(saveShippingInfo(shippingInfo));
+    try {
+      const { data } = await axios.post(
+        `${server}/api/v1/payment/createRazorpay`,
+        {
+          amount: total,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      navigate("/payment", {
+        state: {
+          id: data.id,
+        },
+      });
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong");
+    }
+  };
+
   return (
     <div className="shipping">
-      <button className="backBtn"
+      <button
+        className="backBtn"
         onClick={() => {
           navigate(-1);
         }}
       >
         <BiArrowBack />
       </button>
-      <form>
+      <form onSubmit={submitHandler}>
         <h1>Shipping Address</h1>
         <input
           required
