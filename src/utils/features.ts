2e3e231a7d7541b5cf0b5c7e751e3c44ee4ec1bd
Aman Kumar <amankarguwal0@gmail.com>
2024-10-12 13:42:07 +0530

    feat: Add utility functions for handling API responses and getting last months
    This commit adds utility functions to handle API responses and display toast messages based on the response. It also includes a function to get the names of the last 6 and 12 months. These utility functions will be used in various parts of the application.

---
+++ src/utils/features.ts
@@ -0,0 +1,48 @@
+import { SerializedError } from "@reduxjs/toolkit";
+import { FetchBaseQueryError } from "@reduxjs/toolkit/query";
+import { MessageResponse } from "../types/api-types";
+import { NavigateFunction } from "react-router-dom";
+import toast from "react-hot-toast";
+
+export type ResType =
+  | {
+      data: MessageResponse;
+    }
+  | {
+      error: FetchBaseQueryError | SerializedError;
+    };
+
+export const responseToast = (
+  res: ResType,
+  navigate: NavigateFunction | null,
+  url: string
+) => {
+  if ("data" in res) {
+    toast.success(res.data.message);
+    navigate?.(url);
+  } else {
+    const error = res.error as FetchBaseQueryError;
+    const data = error.data as MessageResponse;
+    toast.error(data.message);
+  }
+};
+
+export const getLastMonths = () => {
+  const date = new Date(new Date().getFullYear(), new Date().getMonth());
+
+  const last6Months: string[] = [];
+  for (let i = 0; i < 6; i++) {
+    const newDate = new Date(date.getFullYear(), date.getMonth() - i);
+    const monthName = newDate.toLocaleString("default", { month: "long" });
+    last6Months.unshift(monthName);
+  }
+
+  const last12Months: string[] = [];
+  for (let i = 0; i < 12; i++) {
+    const newDate = new Date(date.getFullYear(), date.getMonth() - i);
+    const monthName = newDate.toLocaleString("default", { month: "long" });
+    last12Months.unshift(monthName);
+  }
+
+  return { last6Months, last12Months };
+};
