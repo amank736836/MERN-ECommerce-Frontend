c6bdea7178f3b62eb499226eb416ca18071ad3e4
Aman Kumar <amankarguwal0@gmail.com>
2024-10-12 13:33:11 +0530

    refactor: Update CartItem component to use Redux for state management

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,41 +1,60 @@
 import { FaTrash } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { server } from "../redux/store";
+import { useDispatch } from "react-redux";
+import { CartItem as CartItemProps } from "../types/types";
+import toast from "react-hot-toast";
+import { addToCart, removeCartItem } from "../redux/reducer/cartReducer";
 
-type CartItemProps = {
-  productId: string;
-  photo: string;
-  name: string;
-  price: number;
-  quantity: number;
-  stock: number;
-};
+const CartItem = ({ cartItem }: { cartItem: CartItemProps }) => {
+  const { productId, photo, name, price, quantity, stock } = cartItem;
 
-const CartItem = ({
-  productId,
-  photo,
-  name,
-  price,
-  quantity,
-  stock,
-}: CartItemProps) => {
-    console.log(photo);
+  const dispatch = useDispatch();
+
+  const incrementHandler = () => {
+    if (stock < 1) {
+      return toast.error("Out of Stock");
+    } else if (quantity === stock) {
+      return toast.error("Maximum quantity reached");
+    }
+    toast.success("Added to Cart");
+    dispatch(
+      addToCart({
+        ...cartItem,
+        quantity: quantity + 1,
+      })
+    );
+  };
+
+  const decrementHandler = () => {
+    if (quantity === 1) {
+      return toast.error("Minimum quantity reached");
+    }
+    return dispatch(addToCart({ ...cartItem, quantity: quantity - 1 }));
+  };
+
+  const removeHandler = () => {
+    dispatch(removeCartItem(productId));
+  };
+
   return (
     <div className="cartItem">
-      <img src={photo} alt={name} />
+      <img src={`${server}/${photo}`} alt={name} />
       <article>
-        <Link to={`/product/${productId}`}>{name}</Link>
-        <span>
-            ₹{price}
-        </span>
+        <Link to={`/product${productId}`}>{name}</Link>
+        <span>₹{price}</span>
       </article>
       <div>
-        <button>-</button>
-        <p>
-            {quantity} 
-        </p>
-        <button>+</button>
+        <button
+          onClick={() => decrementHandler()}
+          onDoubleClick={() => removeHandler()}
+        >
+          -
+        </button>
+        <p>{quantity}</p>
+        <button onClick={() => incrementHandler()}>+</button>
       </div>
-      <button>
+      <button onClick={() => removeHandler()}>
         <FaTrash />
       </button>
     </div>
