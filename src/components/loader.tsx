241f4c745305c7a813584a127a48949ffc19f1ad
Aman Kumar <amankarguwal0@gmail.com>
2024-10-12 13:34:11 +0530

    refactor: Update Loader component and add SkeletonLoader
    - Refactor the Loader component to use arrow function syntax and remove unnecessary div wrapping
    - Add a new SkeletonLoader component with customizable props for creating loading skeletons

--- src/components/loader.tsx
+++ src/components/loader.tsx
@@ -1,10 +1,52 @@
-
 const Loader = () => {
-  return (
-    <div>
-      Loading...
-    </div>
-  )
+  return <div>Loading...</div>;
+};
+
+export default Loader;
+
+interface SkeletonProps {
+  width?: string;
+  height?: string;
+  margin?: string;
+  padding?: string;
+  length?: number;
+  maxWidth?: string;
+  flexDir?:
+    | "row"
+    | "column"
+    | "unset"
+    | "row-reverse"
+    | "column-reverse"
+    | undefined;
 }
 
-export default Loader
+export const SkeletonLoader = ({
+  width = "unset",
+  height = "unset",
+  flexDir = "unset",
+  margin = "unset",
+  padding = "unset",
+  maxWidth = "unset",
+
+  length = 3,
+}: SkeletonProps) => {
+  const skeletons = Array.from({ length }, (_, index) => (
+    <div
+      key={index}
+      className="skeleton-shape"
+      style={{ width, height, maxWidth }}
+    ></div>
+  ));
+  return (
+    <div
+      className="skeleton-loader"
+      style={{
+        flexDirection: flexDir,
+        margin: margin,
+        padding: padding,
+      }}
+    >
+      {skeletons}
+    </div>
+  );
+};
