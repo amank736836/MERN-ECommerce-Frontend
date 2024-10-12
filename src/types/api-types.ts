1fb06a513dd5df928ef655ded6386286be90f2d9
Aman Kumar <amankarguwal0@gmail.com>
2024-10-12 13:41:47 +0530

    feat: Add API types for managing users, products, orders, and statistics

---
+++ src/types/api-types.ts
@@ -0,0 +1,138 @@
+import {
+  Bar,
+  CartItem,
+  Line,
+  Order,
+  Pie,
+  Product,
+  ShippingInfo,
+  Stats,
+  User,
+} from "./types";
+
+export type CustomError = {
+  status: number;
+  data: {
+    message: string;
+    success: boolean;
+  };
+};
+
+export type MessageResponse = {
+  success: boolean;
+  message: string;
+};
+
+export type AllUsersResponse = {
+  success: boolean;
+  message: string;
+  users: User[];
+};
+
+export type UserResponse = {
+  success: boolean;
+  message: string;
+  user: User;
+};
+
+export type deleteUserRequest = {
+  userId: string;
+  id: string;
+};
+
+export type ProductResponse = {
+  success: boolean;
+  message: string;
+  products: Product[];
+};
+
+export type SingleProductResponse = {
+  success: boolean;
+  message: string;
+  product: Product;
+};
+
+export type CategoryResponse = {
+  success: boolean;
+  message: string;
+  categories: string[];
+};
+
+export type searchProductsResponse = ProductResponse & {
+  totalPage: number;
+  categories: string[];
+};
+
+export type searchProductsRequest = {
+  search?: string;
+  price?: number;
+  category?: string;
+  sort?: string;
+  page?: number;
+};
+
+export type NewProductRequest = {
+  id: string;
+  formData: FormData;
+};
+
+export type UpdateProductRequest = NewProductRequest & {
+  productId: string;
+};
+
+export type DeleteProductRequest = {
+  id: string;
+  productId: string;
+};
+
+export type AllOrdersResponse = {
+  success: boolean;
+  message: string;
+  orders: Order[];
+};
+
+export type OrderDetailsResponse = {
+  success: boolean;
+  message: string;
+  order: Order;
+};
+
+export type NewOrderRequest = {
+  orderItems: CartItem[];
+  subtotal: number;
+  shippingCharges: number;
+  tax: number;
+  discount: number;
+  total: number;
+  shippingInfo: ShippingInfo;
+  user: string;
+};
+
+export type UpdateOrderRequest = {
+  id: string;
+  orderId: string;
+};
+
+export type StatsResponse = {
+  success: boolean;
+  message: string;
+  stats: Stats;
+};
+
+export type BarResponse = {
+  success: boolean;
+  message: string;
+  charts: Bar;
+};
+
+export type PieResponse = {
+  success: boolean;
+  message: string;
+  charts: Pie;
+};
+
+export type LineResponse = {
+  success: boolean;
+  message: string;
+  charts: Line;
+};
