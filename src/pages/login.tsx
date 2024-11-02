f4160d5a0e57a909d553d13f495b564b0693b7e2
Aman Kumar <amankarguwal0@gmail.com>
2024-11-02 14:34:59 +0530

    Refactor login.tsx to add loading state and display loader during sign-in

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -6,6 +6,7 @@ import { useNavigate } from "react-router-dom";
 import { auth } from "../firebase";
 import { useLoginMutation } from "../redux/api/userAPI";
 import { responseToast } from "../utils/features";
+import Loader from "../components/admin/Loader";
 
 const Login = () => {
   const navigate = useNavigate();
@@ -14,8 +15,12 @@ const Login = () => {
   const [date, setDate] = useState("");
 
   const [login] = useLoginMutation();
+
+  const [Loading, setLoading] = useState<"loading" | "">();
+
   const loginHandler = async () => {
     try {
+      setLoading("loading");
       const provider = new GoogleAuthProvider();
       const { user } = await signInWithPopup(auth, provider);
       const res = await login({
@@ -30,12 +35,21 @@ const Login = () => {
 
       responseToast(res, navigate, "/");
     } catch (error) {
+      setLoading("");
       console.error(error);
       toast.error("Sign in failed");
     }
   };
 
-  return (
+  return Loading ? (
+    <div>
+      <center>
+
+      <h1>Sign in with Google in the popup window</h1>
+      </center>
+      <Loader />
+    </div>
+  ) : (
     <div className="login">
       <main>
         <h1 className="heading">Login</h1>
