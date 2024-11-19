c0fdb780f3f7092efe2f213e63b74d9103b0574f
Aman Kumar <amankarguwal0@gmail.com>
2024-11-19 21:17:02 +0530

    feat: Add setShowModal prop to AppSidebar and close modal on list item click

--- src/components/admin/AdminSidebar/Sidebar/AppSidebar.tsx
+++ src/components/admin/AdminSidebar/Sidebar/AppSidebar.tsx
@@ -5,14 +5,16 @@ import { Location } from "react-router-dom";
 export const AppSidebar = ({
   apps,
   location,
+  setShowModal,
 }: {
   apps: { name: string; icon: IconType; url: string }[];
   location: Location;
+  setShowModal: (value: boolean) => void;
 }) => {
   return (
     <div>
       <h5>Apps</h5>
-      <ul>
+      <ul onClick={() => setShowModal(false)}>
         {apps.map((item, index) => (
           <Li
             key={index}
