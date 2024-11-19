afeb8c8e82d2f985f1b786f0c43e74e16cd9b37b
Aman Kumar <amankarguwal0@gmail.com>
2024-11-19 21:17:08 +0530

    feat: Add setShowModal prop to ChartSidebar and close modal on list item click

--- src/components/admin/AdminSidebar/Sidebar/ChartSidebar.tsx
+++ src/components/admin/AdminSidebar/Sidebar/ChartSidebar.tsx
@@ -5,14 +5,16 @@ import { Location } from "react-router-dom";
 export const ChartSidebar = ({
   charts,
   location,
+  setShowModal,
 }: {
   charts: { name: string; icon: IconType; url: string }[];
   location: Location;
+  setShowModal: (value: boolean) => void;
 }) => {
   return (
     <div>
       <h5>Charts</h5>
-      <ul>
+      <ul onClick={() => setShowModal(false)}>
         {charts.map((item, index) => (
           <Li
             key={index}
