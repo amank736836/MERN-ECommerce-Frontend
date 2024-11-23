3b4e399a397db4fcef272948e8c8c091932ad7ce
Aman Kumar <amankarguwal0@gmail.com>
2024-11-23 19:10:20 +0530

    refactor: Update AppItems to include new icons and add New Product entry

--- src/components/admin/AdminSidebar/Items/AppsItems.tsx
+++ src/components/admin/AdminSidebar/Items/AppsItems.tsx
@@ -1,16 +1,22 @@
-import { FaGamepad, FaStopwatch } from "react-icons/fa";
-import { RiCoupon3Fill } from "react-icons/ri";
+import { BiSolidCoupon } from "react-icons/bi";
+import { FaGamepad, FaProductHunt, FaStopwatch } from "react-icons/fa";
 
 export const AppItems = [
+  {
+    name: "New Coupon",
+
+    icon: BiSolidCoupon,
+    url: "/admin/app/coupon",
+  },
+  {
+    name: "New Product",
+    icon: FaProductHunt,
+    url: "/admin/app/product",
+  },
   {
     name: "Stopwatch",
     icon: FaStopwatch,
     url: "/admin/app/stopwatch",
-  },
-  {
-    name: "Coupon",
-    icon: RiCoupon3Fill,
-    url: "/admin/app/coupon",
   },
   {
     name: "Toss",
