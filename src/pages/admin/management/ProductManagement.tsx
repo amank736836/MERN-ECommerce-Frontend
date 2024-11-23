436921e27d646a115e5ca5d01615ad04e999f4c1
Aman Kumar <amankarguwal0@gmail.com>
2024-11-23 22:40:59 +0530

    refactor: Replace ImageClickHandler with direct navigation in ProductManagement component

--- src/pages/admin/management/ProductManagement.tsx
+++ src/pages/admin/management/ProductManagement.tsx
@@ -184,10 +184,6 @@ const ProductManagement = () => {
     }
   };
 
-  const ImageClickHandler = (id: string) => {
-    navigate(`/product/${id}`);
-  };
-
   if (isError || error) {
     return <Navigate to="/admin/products" />;
   }
@@ -201,7 +197,7 @@ const ProductManagement = () => {
           <SkeletonLoader length={2} width="500px" height="85vh" />
         ) : (
           <>
-            <section onClick={() => ImageClickHandler(data?.product._id!)}>
+            <section onClick={() => navigate(`/product/${data?.product._id}`)}>
               <strong>ID - {data?.product._id}</strong>
               <img src={`${photos[0].url}`} alt="Product" />
               <p>{name}</p>
