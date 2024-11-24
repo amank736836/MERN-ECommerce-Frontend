62986a4b16a4c75402be3a2554d54295ae6b9644
Aman Kumar <amankarguwal0@gmail.com>
2024-11-24 23:55:31 +0530

    fix: Refactor ProductLoader component to improve skeleton loader layout and dimensions

--- src/components/Loaders/ProductLoader.tsx
+++ src/components/Loaders/ProductLoader.tsx
@@ -30,16 +30,29 @@ const ProductLoader = () => {
           // border: "1px solid blue",
           display: "flex",
           flexDirection: "column",
-          gap: "4rem",
+          gap: "1rem",
           padding: "2rem",
         }}
       >
-        <SkeletonLoader width="100%" length={3} />
-        <SkeletonLoader width="100%" length={2} flexDir="column" />
-        <SkeletonLoader width="100%" length={3} />
-        <SkeletonLoader width="100%" length={3} flexDir="column" />
-        <SkeletonLoader width="100%" length={3} />
-        <SkeletonLoader width="100%" length={2} flexDir="column" />
+        <SkeletonLoader width="25%" length={4} flexDir="row" height="2.5rem" />
+        <SkeletonLoader width="50%" length={2} flexDir="row" height="2.5rem" />
+        <SkeletonLoader width="36%" length={3} flexDir="row" height="2.5rem" />
+        <SkeletonLoader width="50%" length={2} flexDir="row" height="2.5rem" />
+        <SkeletonLoader width="36%" length={3} flexDir="row" height="2.5rem" />
+        <SkeletonLoader width="25%" length={4} flexDir="row" height="2.5rem" />
+        <SkeletonLoader width="25%" length={4} flexDir="row" height="2.5rem" />
+        <SkeletonLoader width="50%" length={2} flexDir="row" height="2.5rem" />
+        <SkeletonLoader width="36%" length={3} flexDir="row" height="2.5rem" />
+        <SkeletonLoader width="50%" length={2} flexDir="row" height="2.5rem" />
+        <SkeletonLoader width="36%" length={3} flexDir="row" height="2.5rem" />
+        <SkeletonLoader width="25%" length={4} flexDir="row" height="2.5rem" />
+
+        <SkeletonLoader
+          width="100%"
+          length={1}
+          flexDir="column"
+          height="2rem"
+        />
       </section>
     </div>
   );
