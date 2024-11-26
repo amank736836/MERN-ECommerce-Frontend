714a633d9a2e6227223b817c3e8b04db6180f915
Aman Kumar <amankarguwal0@gmail.com>
2024-11-26 13:46:19 +0530

    fix: Integrate reviews fetching and loading states in product details page

--- src/pages/productDetails.tsx
+++ src/pages/productDetails.tsx
@@ -5,8 +5,13 @@ import { FaArrowLeftLong, FaArrowRightLong } from "react-icons/fa6";
 import { useDispatch } from "react-redux";
 import { Navigate, useNavigate, useParams } from "react-router-dom";
 import ProductLoader from "../components/Loaders/ProductLoader";
+import ReviewLoader from "../components/Loaders/ReviewLoader";
 import Ratings from "../components/Ratings";
-import { useProductDetailsQuery } from "../redux/api/productAPI";
+import ReviewCard from "../components/ReviewCard";
+import {
+  useAllReviewsOfProductQuery,
+  useProductDetailsQuery,
+} from "../redux/api/productAPI";
 import { addToCart } from "../redux/reducer/cartReducer";
 import { CustomError } from "../types/api-types";
 import { Product } from "../types/types";
@@ -14,20 +19,35 @@ import { Product } from "../types/types";
 const productDetails = () => {
   const params = useParams();
 
-  const { data, isLoading, isError, error } = useProductDetailsQuery(
-    params.id!
-  );
+  const {
+    data: productData,
+    isLoading: isProductLoading,
+    isError: isProductError,
+    error: productError,
+  } = useProductDetailsQuery(params.id!);
+  const {
+    data: reviewsData,
+    isLoading: isReviewsLoading,
+    isError: isReviewsError,
+    error: reviewsError,
+  } = useAllReviewsOfProductQuery(params.id!);
 
   useEffect(() => {
-    if (isError || error) {
-      const err = error as CustomError;
+    if (isProductError || productError) {
+      const err = productError as CustomError;
       err.data?.message
         ? toast.error(err.data.message)
         : toast.error("Failed to fetch product details");
     }
-  }, [isError, error]);
+    if (isReviewsError || reviewsError) {
+      const err = reviewsError as CustomError;
+      err.data?.message
+        ? toast.error(err.data.message)
+        : toast.error("Failed to fetch product details");
+    }
+  }, [isProductError, productError, isReviewsError, reviewsError]);
 
-  if (isError || error) {
+  if (isProductError || productError || isReviewsError || reviewsError) {
     return <Navigate to="/" />;
   }
 
@@ -35,7 +55,7 @@ const productDetails = () => {
 
   return (
     <div className="productDetails">
-      {isLoading ? (
+      {isProductLoading ? (
         <ProductLoader />
       ) : (
         <>
@@ -44,7 +64,9 @@ const productDetails = () => {
               <Slider
                 objectFit="scale-down"
                 onClick={() => setCarouselOpen(true)}
-                images={data?.product.photos.map((photo) => photo.url) || []}
+                images={
+                  productData?.product.photos.map((photo) => photo.url) || []
+                }
                 showThumbnails
                 // showNav={true}
                 showDots
@@ -66,28 +88,37 @@ const productDetails = () => {
                   NextButton={NextButton}
                   PrevButton={PrevButton}
                   setIsOpen={setCarouselOpen}
-                  images={data?.product.photos.map((photo) => photo.url) || []}
+                  images={
+                    productData?.product.photos.map((photo) => photo.url) || []
+                  }
                 />
               )}
             </section>
-            {data ? (
+            {productData ? (
               <section>
-                <code>{data.product.category}</code>
-                <h1>{data.product.name}</h1>
+                <code>{productData.product.category}</code>
+                <h1>{productData.product.name}</h1>
                 <em
                   style={{ display: "flex", alignItems: "center", gap: "1rem" }}
                 >
-                  <Ratings value={data.product.averageRating} />
-                  {data.product.numOfReviews} Reviews
+                  <Ratings value={productData.product.averageRating} />
+                  {productData.product.numOfReviews} Reviews
                 </em>
-                <h3>₹{data.product.price}</h3>
-                <CustomizedButtons product={data.product} />
-                <p>{data.product.description}</p>
+                <h3>₹{productData.product.price}</h3>
+                <CustomizedButtons product={productData.product} />
+                <p>{productData.product.description}</p>
               </section>
             ) : null}
           </main>
           <section>
             <h1>Reviews</h1>
+            <div>
+              {isReviewsLoading ? (
+                <ReviewLoader />
+              ) : (
+                <ReviewCard reviewsData={reviewsData} />
+              )}
+            </div>
           </section>
         </>
       )}
