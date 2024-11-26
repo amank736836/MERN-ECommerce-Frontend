e83c2c1954bfee4585cbe5b5f7f1042a61d7c061
Aman Kumar <amankarguwal0@gmail.com>
2024-11-26 13:46:32 +0530

    feat: Add ReviewLoader component for loading state representation

---
+++ src/components/Loaders/ReviewLoader.tsx
@@ -0,0 +1,80 @@
+import { SkeletonLoader } from "./SkeletonLoader";
+
+const ReviewLoader = () => {
+  return (
+    <>
+      <SkeletonLoader
+        width="5vw"
+        height="10vh"
+        length={1}
+        flexDir="row"
+        margin="1rem 0"
+      />
+      <SkeletonLoader
+        width="10vw"
+        height="10px"
+        length={3}
+        flexDir="column"
+        margin="1rem 0"
+      />
+      <SkeletonLoader
+        width="5vw"
+        height="10vh"
+        length={1}
+        flexDir="row"
+        margin="1rem 0"
+      />
+      <SkeletonLoader
+        width="9vw"
+        height="10px"
+        length={3}
+        flexDir="column"
+        margin="1rem 0"
+      />
+      <SkeletonLoader
+        width="5vw"
+        height="10vh"
+        length={1}
+        flexDir="row"
+        margin="1rem 0"
+      />
+      <SkeletonLoader
+        width="10vw"
+        height="10px"
+        length={3}
+        flexDir="column"
+        margin="1rem 0"
+      />
+      <SkeletonLoader
+        width="5vw"
+        height="10vh"
+        length={1}
+        flexDir="row"
+        margin="1rem 0"
+      />
+      <SkeletonLoader
+        width="9vw"
+        height="10px"
+        length={3}
+        flexDir="column"
+        margin="1rem 0"
+      />
+      <SkeletonLoader
+        width="5vw"
+        height="10vh"
+        length={1}
+        flexDir="row"
+        margin="1rem 0"
+      />
+      <SkeletonLoader
+        width="10vw"
+        height="10px"
+        length={3}
+        flexDir="column"
+        margin="1rem 0"
+      />
+    </>
+  );
+};
+
+export default ReviewLoader;
