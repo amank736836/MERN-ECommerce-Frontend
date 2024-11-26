2edf1985f2265755a0eab428dbebfefef7c8eb77
Aman Kumar <amankarguwal0@gmail.com>
2024-11-27 00:48:19 +0530

    refactor: Adjust ProductLoader height and optimize skeleton loader lengths for improved loading experience

--- src/components/Loaders/ProductLoader.tsx
+++ src/components/Loaders/ProductLoader.tsx
@@ -6,7 +6,7 @@ const ProductLoader = () => {
       style={{
         display: "flex",
         gap: "2rem",
-        height: "80vh",
+        height: "60vh",
         border: "1px solid #f1f1f1",
       }}
     >
@@ -34,24 +34,16 @@ const ProductLoader = () => {
           padding: "2rem",
         }}
       >
-        <SkeletonLoader width="25%" length={4} flexDir="row" height="2.5rem" />
-        <SkeletonLoader width="50%" length={2} flexDir="row" height="2.5rem" />
+        <SkeletonLoader width="25%" length={3} flexDir="row" height="2.5rem" />
+        <SkeletonLoader width="50%" length={1} flexDir="row" height="2.5rem" />
         <SkeletonLoader width="36%" length={3} flexDir="row" height="2.5rem" />
-        <SkeletonLoader width="50%" length={2} flexDir="row" height="2.5rem" />
+        <SkeletonLoader width="50%" length={1} flexDir="row" height="2.5rem" />
         <SkeletonLoader width="36%" length={3} flexDir="row" height="2.5rem" />
-        <SkeletonLoader width="25%" length={4} flexDir="row" height="2.5rem" />
-        <SkeletonLoader width="25%" length={4} flexDir="row" height="2.5rem" />
-        <SkeletonLoader width="50%" length={2} flexDir="row" height="2.5rem" />
-        <SkeletonLoader width="36%" length={3} flexDir="row" height="2.5rem" />
-        <SkeletonLoader width="50%" length={2} flexDir="row" height="2.5rem" />
-        <SkeletonLoader width="36%" length={3} flexDir="row" height="2.5rem" />
-        <SkeletonLoader width="25%" length={4} flexDir="row" height="2.5rem" />
-
         <SkeletonLoader
           width="100%"
-          length={1}
+          length={5}
           flexDir="column"
-          height="2rem"
+          height="2.5rem"
         />
       </section>
     </div>
