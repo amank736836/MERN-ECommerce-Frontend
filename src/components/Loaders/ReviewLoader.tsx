1a26fa2651d0092f39612469b5dd58c65cc738e6
Aman Kumar <amankarguwal0@gmail.com>
2024-11-27 18:29:30 +0530

    refactor: Clean up ReviewLoader component structure for improved readability

--- src/components/Loaders/ReviewLoader.tsx
+++ src/components/Loaders/ReviewLoader.tsx
@@ -3,88 +3,84 @@ import { SkeletonLoader } from "./SkeletonLoader";
 const ReviewLoader = () => {
   return (
     <>
-      {/* <div> */}
-        <article
-        style={
-          {
-            display: "flex",
-            flexDirection: "row",
-            gap: "1rem",
-          }
-        }
-        >
-          <SkeletonLoader
-            width="5vw"
-            height="10vh"
-            length={1}
-            flexDir="row"
-            margin="1rem 0"
-          />
-          <SkeletonLoader
-            width="12vw"
-            height="10px"
-            length={3}
-            flexDir="column"
-            margin="1rem 0"
-          />
-          <SkeletonLoader
-            width="5vw"
-            height="10vh"
-            length={1}
-            flexDir="row"
-            margin="1rem 0"
-          />
-          <SkeletonLoader
-            width="12vw"
-            height="10px"
-            length={3}
-            flexDir="column"
-            margin="1rem 0"
-          />
-          <SkeletonLoader
-            width="5vw"
-            height="10vh"
-            length={1}
-            flexDir="row"
-            margin="1rem 0"
-          />
-          <SkeletonLoader
-            width="12vw"
-            height="10px"
-            length={3}
-            flexDir="column"
-            margin="1rem 0"
-          />
-          <SkeletonLoader
-            width="5vw"
-            height="10vh"
-            length={1}
-            flexDir="row"
-            margin="1rem 0"
-          />
-          <SkeletonLoader
-            width="12vw"
-            height="10px"
-            length={3}
-            flexDir="column"
-            margin="1rem 0"
-          />
-          <SkeletonLoader
-            width="5vw"
-            height="10vh"
-            length={1}
-            flexDir="row"
-            margin="1rem 0"
-          />
-          <SkeletonLoader
-            width="12vw"
-            height="10px"
-            length={3}
-            flexDir="column"
-            margin="1rem 0"
-          />
-        </article>
-      {/* </div> */}
+      <article
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          gap: "1rem",
+        }}
+      >
+        <SkeletonLoader
+          width="5vw"
+          height="10vh"
+          length={1}
+          flexDir="row"
+          margin="1rem 0"
+        />
+        <SkeletonLoader
+          width="12vw"
+          height="10px"
+          length={3}
+          flexDir="column"
+          margin="1rem 0"
+        />
+        <SkeletonLoader
+          width="5vw"
+          height="10vh"
+          length={1}
+          flexDir="row"
+          margin="1rem 0"
+        />
+        <SkeletonLoader
+          width="12vw"
+          height="10px"
+          length={3}
+          flexDir="column"
+          margin="1rem 0"
+        />
+        <SkeletonLoader
+          width="5vw"
+          height="10vh"
+          length={1}
+          flexDir="row"
+          margin="1rem 0"
+        />
+        <SkeletonLoader
+          width="12vw"
+          height="10px"
+          length={3}
+          flexDir="column"
+          margin="1rem 0"
+        />
+        <SkeletonLoader
+          width="5vw"
+          height="10vh"
+          length={1}
+          flexDir="row"
+          margin="1rem 0"
+        />
+        <SkeletonLoader
+          width="12vw"
+          height="10px"
+          length={3}
+          flexDir="column"
+          margin="1rem 0"
+        />
+        <SkeletonLoader
+          width="5vw"
+          height="10vh"
+          length={1}
+          flexDir="row"
+          margin="1rem 0"
+        />
+        <SkeletonLoader
+          width="12vw"
+          height="10px"
+          length={3}
+          flexDir="column"
+          margin="1rem 0"
+        />
+      </article>
     </>
   );
 };
