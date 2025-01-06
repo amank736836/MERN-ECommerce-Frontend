0e4b36b49cbb45992647a6056236697e95bbdb04
Aman Kumar <amankarguwal0@gmail.com>
2025-01-06 23:55:17 +0530

    fix: Update Firebase configuration with production credentials

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,13 +3,13 @@ import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API,
-  authDomain: import.meta.env.VITE_FIREBASE_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyAapSLmyk5u4dNM4_99u7XHd4EJcnfiCao",
+  authDomain: "mern-ecommerce-ak-2024.firebaseapp.com",
+  projectId: "mern-ecommerce-ak-2024",
+  storageBucket: "mern-ecommerce-ak-2024.appspot.com",
+  messagingSenderId: "638425419027",
+  appId: "1:638425419027:web:eabb8e9a9bfbf03c7e5ee6",
+  measurementId: "G-SD80TMKLS2",
 };
 
 export const app = initializeApp(firebaseConfig);
